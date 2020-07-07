08a5a39dd8be3a80f2db89e267b69f006944d4bf
MarkR1966 <markrafferty27@gmail.com>
2020-07-07 10:55:42 +0100

    added navbar and another page

--- app.py
+++ app.py
@@ -2,17 +2,33 @@ from flask import Flask
 from flask import render_template
 app = Flask(__name__)
 
-
+dummyData = [
+    {
+        "f_name": "Mark",
+        "l_name": "Rafferty",
+        "title": "Mr",
+        "content": "Blah blah blah"
+    },
+    {
+        "f_name": "Jim",
+        "l_name": "Jones",
+        "title": "Mr",
+        "content": "More content"
+    }
+]
 @app.route('/')
 @app.route('/home')
-def hello_world():
-    return render_template('homepage.html', title='Homepage')
+def home():
+    return render_template('homepage.html', title='Homepage', posts=dummyData)
 
 
 @app.route('/about')
 def about():
     return render_template('about.html', title='About')
 
+@app.route('/another')
+def another():
+    return render_template('another.html', title='Another')
 
 if __name__ == '__main__':
     app.run()
